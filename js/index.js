62f94c2de0f2a2297684e578bb7d1eecbbaa70ac
abhishekballary <abhishekballery@gmail.com>
2021-12-27 20:01:21 +0530

    darkModeAdded

--- js/index.js
+++ js/index.js
@@ -4,3 +4,26 @@ function onLoadFunction(){
     document.querySelector(`.bar${i}`).style.width = el.attributes[1].value
    })
 }
+
+
+function darkMode(el){
+	if(el.classList.contains('fa-sun')){
+	el.classList.toggle("fa-moon")
+	// document.body.style.backgroundColor = '#000000 !important'
+	el.style.color = '#ffffff'
+	document.body.style.transition = "all 2s"
+	// document.body.style.color = 'blue'
+	document.body.classList.remove('body')
+	document.body.classList.toggle('darkMode')
+	}
+	else{
+		el.classList.toggle("fa-sun")
+	// document.body.style.backgroundColor = '#ffffff !important'
+	// el.style.color = '#000000'
+	document.body.style.transition = "all 2s"
+	// document.body.style.color = 'black'
+	document.body.classList.remove('darkMode')
+	document.body.classList.toggle('body')
+	}
+	
+	}
